16b1d950cce60d432eb03905e7dec7acda367b3d
Xeraph <jeongdo.dev@gmail.com>
2025-02-11 00:03:37 +0900

    Revert "Feat blog page"

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-    ],
     domains: ["www.animal.go.kr", "d3vt348duxk7vk.cloudfront.net"],
   },
 };
--- src/app/posts/[...slug]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function PostLayout({ children }: { children: ReactNode }) {
-  return <div className="flex justify-center w-full">
-    <div className="md:w-[70%] w-full">
-      {children}
-    </div>
-    </div>;
-}
